a1cc76e5e0d7428650bd946ed426e2ee923221ca
SukhoiGit <3103846585@qq.com>
2025-11-28 11:18:00 +0800

    修复@EnableElasticTp可插拔配置注解未生效问题

--- starter/apollo-spring-boot-starter/src/main/java/com/artemis/elastictp/config/apollo/starter/configuration/ApolloAutoConfiguration.java
+++ starter/apollo-spring-boot-starter/src/main/java/com/artemis/elastictp/config/apollo/starter/configuration/ApolloAutoConfiguration.java
@@ -2,16 +2,19 @@ package com.artemis.elastictp.config.apollo.starter.configuration;
 
 import com.artemis.elastictp.config.apollo.starter.refresher.ApolloRefresherHandler;
 
-import com.artemis.elastictp.core.notification.service.DingTalkMessageService;
 import com.artemis.elastictp.core.notification.service.NotifierDispatcher;
 import com.artemis.elastictp.spring.base.configuration.BootstrapConfigProperties;
+import com.artemis.elastictp.spring.base.enable.MarkerConfiguration;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 /**
  * Apollo 配置中心自动装配
  */
 @Configurable
+@ConditionalOnBean(MarkerConfiguration.Marker.class)
+@ConditionalOnProperty(prefix = BootstrapConfigProperties.PREFIX, value = "enable", matchIfMissing = true, havingValue = "true")
 public class ApolloAutoConfiguration {
 
     public ApolloRefresherHandler apolloRefresherHandler(BootstrapConfigProperties properties,
--- starter/nacos-cloud-spring-boot-starter/src/main/java/com/artemis/elastictp/config/nacos/cloud/starter/configuration/NacosCloudAutoConfiguration.java
+++ starter/nacos-cloud-spring-boot-starter/src/main/java/com/artemis/elastictp/config/nacos/cloud/starter/configuration/NacosCloudAutoConfiguration.java
@@ -4,17 +4,18 @@ import com.artemis.elastictp.config.nacos.cloud.starter.refresher.NacosCloudRefr
 import com.artemis.elastictp.core.notification.service.DingTalkMessageService;
 import com.artemis.elastictp.core.notification.service.NotifierDispatcher;
 import com.artemis.elastictp.spring.base.configuration.BootstrapConfigProperties;
+import com.artemis.elastictp.spring.base.enable.MarkerConfiguration;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 
 /**
  * Nacos Cloud 版本自动装配
- * <p>
- * 作者：马丁
- * 加项目群：早加入就是优势！500人内部项目群，分享的知识总有你需要的 <a href="https://t.zsxq.com/cw7b9" />
- * 开发时间：2025-04-24
  */
 @Configurable
+@ConditionalOnBean(MarkerConfiguration.Marker.class)
+@ConditionalOnProperty(prefix = BootstrapConfigProperties.PREFIX, value = "enable", matchIfMissing = true, havingValue = "true")
 public class NacosCloudAutoConfiguration {
 
     @Bean
