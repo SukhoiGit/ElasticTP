a44558efc8932955d48ca7095dcc66a14b841d82
SukhoiGit <3103846585@qq.com>
2025-12-09 22:14:22 +0800

    重构Apollo动态线程池组件库

--- starter/apollo-spring-boot-starter/src/main/java/com/artemis/elastictp/config/apollo/starter/refresher/ApolloRefresherHandler.java
+++ starter/apollo-spring-boot-starter/src/main/java/com/artemis/elastictp/config/apollo/starter/refresher/ApolloRefresherHandler.java
@@ -1,58 +1,23 @@
 package com.artemis.elastictp.config.apollo.starter.refresher;
 
-import cn.hutool.core.collection.CollUtil;
-import com.artemis.elastictp.core.executor.ElasticTpRegistry;
-import com.artemis.elastictp.core.executor.ThreadPoolExecutorHolder;
-import com.artemis.elastictp.core.executor.ThreadPoolExecutorProperties;
-import com.artemis.elastictp.core.executor.support.BlockingQueueTypeEnum;
-import com.artemis.elastictp.core.executor.support.RejectedPolicyTypeEnum;
-import com.artemis.elastictp.core.executor.support.ResizableCapacityLinkedBlockingQueue;
+import com.artemis.elastictp.config.common.starter.refresher.AbstractDynamicThreadPoolRefresher;
 import com.artemis.elastictp.spring.base.configuration.BootstrapConfigProperties;
-import com.artemis.elastictp.spring.base.parser.ConfigParserHandler;
 import com.ctrip.framework.apollo.Config;
 import com.ctrip.framework.apollo.ConfigChangeListener;
 import com.ctrip.framework.apollo.ConfigFile;
 import com.ctrip.framework.apollo.ConfigService;
 import com.ctrip.framework.apollo.core.enums.ConfigFileFormat;
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.context.properties.bind.Bindable;
-import org.springframework.boot.context.properties.bind.Binder;
-import org.springframework.boot.context.properties.source.ConfigurationPropertySource;
-import org.springframework.boot.context.properties.source.MapConfigurationPropertySource;
-
-import java.io.IOException;
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.RejectedExecutionHandler;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Apollo 配置中心刷新处理器
  */
 @Slf4j(topic = "OneThreadConfigRefresher")
-@RequiredArgsConstructor
-public class ApolloRefresherHandler implements ApplicationRunner {
+public class ApolloRefresherHandler extends AbstractDynamicThreadPoolRefresher {
 
-    private final BootstrapConfigProperties properties;
-
-    public static final String CHANGE_THREAD_POOL_TEXT = "[{}] Dynamic thread pool parameter changed:"
-            + "\n    corePoolSize: {}"
-            + "\n    maximumPoolSize: {}"
-            + "\n    capacity: {}"
-            + "\n    keepAliveTime: {}"
-            + "\n    rejectedType: {}"
-            + "\n    allowCoreThreadTimeOut: {}";
-    public static final String CHANGE_DELIMITER = "%s => %s";
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        registerListener();
+    public ApolloRefresherHandler(BootstrapConfigProperties properties) {
+        super(properties);
     }
 
     @SneakyThrows
@@ -75,136 +40,7 @@ public class ApolloRefresherHandler implements ApplicationRunner {
             String namespaceItem = namespace.replace("." + configFileType, "");
             ConfigFileFormat configFileFormat = ConfigFileFormat.fromString(configFileType);
             ConfigFile configFile = ConfigService.getConfigFile(namespaceItem, configFileFormat);
-
-            Map<Object, Object> configInfoMap;
-            try {
-                configInfoMap = ConfigParserHandler.getInstance().parseConfig(configFile.getContent(), properties.getConfigFileType());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            ConfigurationPropertySource sources = new MapConfigurationPropertySource(configInfoMap);
-            Binder binder = new Binder(sources);
-            BootstrapConfigProperties refresherProperties = binder.bind(BootstrapConfigProperties.PREFIX, Bindable.ofInstance(properties)).get();
-
-            // 检查远程配置文件是否包含线程池配置
-            if (CollUtil.isEmpty(refresherProperties.getExecutors())) {
-                return;
-            }
-
-            // 刷新动态线程池对象核心参数
-            for (ThreadPoolExecutorProperties remoteProperties : refresherProperties.getExecutors()) {
-                // 检查线程池配置是否发生变化（与当前内存中的配置对比）
-                boolean changed = hasThreadPoolConfigChanged(remoteProperties);
-                if (!changed) {
-                    continue;
-                }
-
-                // 将远程配置应用到线程池，更新相关参数
-                updateThreadPoolFromRemoteConfig(remoteProperties);
-
-                // 线程池参数变更后进行日志打印
-                String threadPoolId = remoteProperties.getThreadPoolId();
-                ThreadPoolExecutorHolder holder = ElasticTpRegistry.getHolder(threadPoolId);
-                ThreadPoolExecutorProperties originalProperties = holder.getExecutorProperties();
-                holder.setExecutorProperties(remoteProperties);
-                log.info(CHANGE_THREAD_POOL_TEXT,
-                        threadPoolId,
-                        String.format(CHANGE_DELIMITER, originalProperties.getCorePoolSize(), remoteProperties.getCorePoolSize()),
-                        String.format(CHANGE_DELIMITER, originalProperties.getMaximumPoolSize(), remoteProperties.getMaximumPoolSize()),
-                        String.format(CHANGE_DELIMITER, originalProperties.getQueueCapacity(), remoteProperties.getQueueCapacity()),
-                        String.format(CHANGE_DELIMITER, originalProperties.getKeepAliveTime(), remoteProperties.getKeepAliveTime()),
-                        String.format(CHANGE_DELIMITER, originalProperties.getRejectedHandler(), remoteProperties.getRejectedHandler()),
-                        String.format(CHANGE_DELIMITER, originalProperties.getAllowCoreThreadTimeOut(), remoteProperties.getAllowCoreThreadTimeOut()));
-            }
+            refreshThreadPoolProperties(configFile.getContent());
         };
-    }
-
-    private boolean hasThreadPoolConfigChanged(ThreadPoolExecutorProperties remoteProperties) {
-        String threadPoolId = remoteProperties.getThreadPoolId();
-        ThreadPoolExecutorHolder holder = ElasticTpRegistry.getHolder(threadPoolId);
-        if (holder == null) {
-            log.warn("No thread pool found for thread pool id: {}", threadPoolId);
-            return false;
-        }
-        ThreadPoolExecutor executor = holder.getExecutor();
-        ThreadPoolExecutorProperties originalProperties = holder.getExecutorProperties();
-
-        return hasDifference(originalProperties, remoteProperties, executor);
-    }
-
-    private void updateThreadPoolFromRemoteConfig(ThreadPoolExecutorProperties remoteProperties) {
-        String threadPoolId = remoteProperties.getThreadPoolId();
-        ThreadPoolExecutorHolder holder = ElasticTpRegistry.getHolder(threadPoolId);
-        ThreadPoolExecutor executor = holder.getExecutor();
-        ThreadPoolExecutorProperties originalProperties = holder.getExecutorProperties();
-
-        Integer remoteCorePoolSize = remoteProperties.getCorePoolSize();
-        Integer remoteMaximumPoolSize = remoteProperties.getMaximumPoolSize();
-        if (remoteCorePoolSize != null && remoteMaximumPoolSize != null) {
-            int originalMaximumPoolSize = executor.getMaximumPoolSize();
-            if (remoteCorePoolSize > originalMaximumPoolSize) {
-                executor.setMaximumPoolSize(remoteMaximumPoolSize);
-                executor.setCorePoolSize(remoteCorePoolSize);
-            } else {
-                executor.setCorePoolSize(remoteCorePoolSize);
-                executor.setMaximumPoolSize(remoteMaximumPoolSize);
-            }
-        } else {
-            if (remoteMaximumPoolSize != null) {
-                executor.setMaximumPoolSize(remoteMaximumPoolSize);
-            }
-            if (remoteCorePoolSize != null) {
-                executor.setCorePoolSize(remoteCorePoolSize);
-            }
-        }
-
-        if (remoteProperties.getAllowCoreThreadTimeOut() != null &&
-                !Objects.equals(remoteProperties.getAllowCoreThreadTimeOut(), originalProperties.getAllowCoreThreadTimeOut())) {
-            executor.allowCoreThreadTimeOut(remoteProperties.getAllowCoreThreadTimeOut());
-        }
-
-        if (remoteProperties.getRejectedHandler() != null &&
-                !Objects.equals(remoteProperties.getRejectedHandler(), originalProperties.getRejectedHandler())) {
-            RejectedExecutionHandler handler = RejectedPolicyTypeEnum.createPolicy(remoteProperties.getRejectedHandler());
-            executor.setRejectedExecutionHandler(handler);
-        }
-
-        if (remoteProperties.getKeepAliveTime() != null &&
-                !Objects.equals(remoteProperties.getKeepAliveTime(), originalProperties.getKeepAliveTime())) {
-            executor.setKeepAliveTime(remoteProperties.getKeepAliveTime(), TimeUnit.SECONDS);
-        }
-
-        // 更新队列容量（仅对 ResizableCapacityLinkedBlockingQueue 生效）
-        if (isQueueCapacityChanged(originalProperties, remoteProperties, executor)) {
-            BlockingQueue<Runnable> queue = executor.getQueue();
-            ResizableCapacityLinkedBlockingQueue<?> resizableQueue = (ResizableCapacityLinkedBlockingQueue<?>) queue;
-            resizableQueue.setCapacity(remoteProperties.getQueueCapacity());
-        }
-    }
-
-    private boolean hasDifference(ThreadPoolExecutorProperties originalProperties, ThreadPoolExecutorProperties remoteProperties, ThreadPoolExecutor executor) {
-        return isChanged(originalProperties.getCorePoolSize(), remoteProperties.getCorePoolSize())
-                || isChanged(originalProperties.getMaximumPoolSize(), remoteProperties.getMaximumPoolSize())
-                || isChanged(originalProperties.getAllowCoreThreadTimeOut(), remoteProperties.getAllowCoreThreadTimeOut())
-                || isChanged(originalProperties.getKeepAliveTime(), remoteProperties.getKeepAliveTime())
-                || isChanged(originalProperties.getRejectedHandler(), remoteProperties.getRejectedHandler())
-                || isQueueCapacityChanged(originalProperties, remoteProperties, executor);
-    }
-
-    private <T> boolean isChanged(T before, T after) {
-        return after != null && !Objects.equals(before, after);
-    }
-
-    private boolean isQueueCapacityChanged(ThreadPoolExecutorProperties originalProperties,
-                                           ThreadPoolExecutorProperties remoteProperties,
-                                           ThreadPoolExecutor executor) {
-        Integer remoteCapacity = remoteProperties.getQueueCapacity();
-        Integer originalCapacity = originalProperties.getQueueCapacity();
-        BlockingQueue<?> queue = executor.getQueue();
-
-        return remoteCapacity != null
-                && !Objects.equals(remoteCapacity, originalCapacity)
-                && Objects.equals(BlockingQueueTypeEnum.RESIZABLE_CAPACITY_LINKED_BLOCKING_QUEUE.getName(), queue.getClass().getSimpleName());
     }
 }
