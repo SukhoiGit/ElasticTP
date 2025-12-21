83cb065c3f27bcda6ed7f127d01c977937f0b3fc
SukhoiGit <3103846585@qq.com>
2025-12-21 16:49:43 +0800

    开发Micrometer指标监控以及完善监控执行机制

--- core/src/main/java/com/artemis/elastictp/core/monitor/ThreadPoolMonitor.java
+++ core/src/main/java/com/artemis/elastictp/core/monitor/ThreadPoolMonitor.java
@@ -1,14 +1,22 @@
 package com.artemis.elastictp.core.monitor;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollectionUtil;
 import com.alibaba.fastjson2.JSON;
+import com.artemis.elastictp.core.config.ApplicationProperties;
+import com.artemis.elastictp.core.config.BootstrapConfigProperties;
 import com.artemis.elastictp.core.executor.ElasticTpExecutor;
 import com.artemis.elastictp.core.executor.ElasticTpRegistry;
 import com.artemis.elastictp.core.executor.ThreadPoolExecutorHolder;
 import com.artemis.elastictp.core.toolkit.ThreadFactoryBuilder;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Tag;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.*;
 
 /**
@@ -17,36 +25,88 @@ import java.util.concurrent.*;
 @Slf4j
 public class ThreadPoolMonitor {
 
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(
-            1,
-            ThreadFactoryBuilder.builder()
-                    .namePrefix("scheduler_thread-pool_monitor")
-                    .build()
-    );
+    private ScheduledExecutorService scheduler;
+    private Map<String, ThreadPoolMonitorDTO> micrometerMonitorCache;
+
+    private static final String METRIC_NAME_PREFIX = "dynamic.thread-pool";
+    private static final String DYNAMIC_THREAD_POOL_ID_TAG = METRIC_NAME_PREFIX + ".id";
+    private static final String APPLICATION_NAME_TAG = "application.name";
 
     /**
      * 启动定时检查任务
      */
     public void start() {
-        // 每30秒检查一次，初始延迟0秒
-        scheduler.scheduleWithFixedDelay(this::logMonitor, 0, 30, TimeUnit.SECONDS);
+        BootstrapConfigProperties.MonitorConfig monitorConfig = BootstrapConfigProperties.getInstance().getMonitorConfig();
+        if (!monitorConfig.getEnable()) {
+            return;
+        }
+
+        // 初始化监控相关资源
+        micrometerMonitorCache = new ConcurrentHashMap<>();
+        scheduler = Executors.newScheduledThreadPool(
+                1,
+                ThreadFactoryBuilder.builder()
+                        .namePrefix("scheduler_thread-pool_monitor")
+                        .build()
+        );
+
+        // 每指定时间检查一次，初始延迟0秒
+        scheduler.scheduleWithFixedDelay(() -> {
+            Collection<ThreadPoolExecutorHolder> holders = ElasticTpRegistry.getAllHolders();
+            for (ThreadPoolExecutorHolder holder : holders) {
+                ThreadPoolMonitorDTO monitorDTO = buildThreadPoolMonitorDTO(holder);
+
+                // 根据采集类型判断
+                if (Objects.equals(monitorConfig.getCollectType(), "log")) {
+                    logMonitor(monitorDTO);
+                } else if (Objects.equals(monitorConfig.getCollectType(), "micrometer")) {
+                    micrometerMonitor(monitorDTO);
+                }
+            }
+        }, 0, monitorConfig.getCollectInterval(), TimeUnit.SECONDS);
     }
 
     /**
      * 停止报警检查
      */
     public void stop() {
-        if (!scheduler.isShutdown()) {
+        if (scheduler != null && !scheduler.isShutdown()) {
             scheduler.shutdown();
         }
     }
 
-    private void logMonitor() {
-        Collection<ThreadPoolExecutorHolder> holders = ElasticTpRegistry.getAllHolders();
-        for (ThreadPoolExecutorHolder holder : holders) {
-            ThreadPoolMonitorDTO monitorDTO = buildThreadPoolMonitorDTO(holder);
-            log.info("[ThreadPool Monitor] {} | Content: {}", holder.getThreadPoolId(), JSON.toJSON(monitorDTO));
+    private void logMonitor(ThreadPoolMonitorDTO monitorDTO) {
+        log.info("[ThreadPool Monitor] {} | Content: {}", monitorDTO.getThreadPoolId(), JSON.toJSON(monitorDTO));
+    }
+
+    private void micrometerMonitor(ThreadPoolMonitorDTO monitorDTO) {
+        String threadPoolId = monitorDTO.getThreadPoolId();
+        ThreadPoolMonitorDTO existingDTO = micrometerMonitorCache.get(threadPoolId);
+        if (existingDTO != null) {
+            BeanUtil.copyProperties(monitorDTO, existingDTO);
+        } else {
+            micrometerMonitorCache.put(threadPoolId, monitorDTO);
+            existingDTO = monitorDTO;
         }
+
+        Iterable<Tag> tags = CollectionUtil.newArrayList(
+                Tag.of(DYNAMIC_THREAD_POOL_ID_TAG, threadPoolId),
+                Tag.of(APPLICATION_NAME_TAG, ApplicationProperties.getApplicationName()));
+
+        Metrics.gauge(metricName("core.size"), tags, existingDTO, ThreadPoolMonitorDTO::getCorePoolSize);
+        Metrics.gauge(metricName("maximum.size"), tags, existingDTO, ThreadPoolMonitorDTO::getMaximumPoolSize);
+        Metrics.gauge(metricName("current.size"), tags, monitorDTO, ThreadPoolMonitorDTO::getActivePoolSize);
+        Metrics.gauge(metricName("largest.size"), tags, monitorDTO, ThreadPoolMonitorDTO::getLargestPoolSize);
+        Metrics.gauge(metricName("active.size"), tags, monitorDTO, ThreadPoolMonitorDTO::getActivePoolSize);
+        Metrics.gauge(metricName("queue.size"), tags, monitorDTO, ThreadPoolMonitorDTO::getWorkQueueSize);
+        Metrics.gauge(metricName("queue.capacity"), tags, monitorDTO, ThreadPoolMonitorDTO::getWorkQueueCapacity);
+        Metrics.gauge(metricName("queue.remaining.capacity"), tags, monitorDTO, ThreadPoolMonitorDTO::getWorkQueueRemainingCapacity);
+        Metrics.gauge(metricName("completed.task.count"), tags, monitorDTO, ThreadPoolMonitorDTO::getCompletedTaskCount);
+        Metrics.gauge(metricName("reject.count"), tags, monitorDTO, ThreadPoolMonitorDTO::getRejectCount);
+    }
+
+    private String metricName(String name) {
+        return String.join(".", METRIC_NAME_PREFIX, name);
     }
 
     @SneakyThrows
@@ -59,6 +119,8 @@ public class ThreadPoolMonitor {
             rejectCount = ((ElasticTpExecutor) executor).getRejectCount().get();
         }
 
+        int workQueueSize = queue.size(); // API 有锁，避免高频率调用
+        int remainingCapacity = queue.remainingCapacity(); // API 有锁，避免高频率调用
         return ThreadPoolMonitorDTO.builder()
                 .threadPoolId(holder.getThreadPoolId())
                 .corePoolSize(executor.getCorePoolSize())
@@ -68,8 +130,9 @@ public class ThreadPoolMonitor {
                 .completedTaskCount(executor.getCompletedTaskCount())  // API 有锁，避免高频率调用
                 .largestPoolSize(executor.getLargestPoolSize())  // API 有锁，避免高频率调用
                 .workQueueName(queue.getClass().getSimpleName())
-                .workQueueCapacity(queue.remainingCapacity())
-                .workQueueSize(queue.size())
+                .workQueueSize(workQueueSize)
+                .workQueueRemainingCapacity(remainingCapacity)
+                .workQueueCapacity(workQueueSize + remainingCapacity)
                 .rejectedHandlerName(executor.getRejectedExecutionHandler().toString())
                 .rejectCount(rejectCount)
                 .build();
